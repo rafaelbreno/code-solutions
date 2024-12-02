541b9eda4f00aeea189d4082e54b6e1757d5322f
Rafael Breno <32229014+rafaelbreno@users.noreply.github.com>
2024-12-02 20:04:57 -0300

    AoC 2024 day 1 - File handling and some cleanup

--- advent_of_code/2024/01/Main.hs
+++ advent_of_code/2024/01/Main.hs
@@ -1,29 +1,40 @@
 import Data.List (sort)
+import System.IO (withFile, IOMode(ReadMode), Handle, hIsEOF, hGetLine)
 
 main :: IO ()
 main = do
-  -- TODO:
-  --  Room for improvement, here the whole file is 
-  --  saved in-memory, instead we could go line by line.
-  file_content <- readFile "input.txt"
-  let results = map processLine (lines file_content)
-  let (leftNumbers, rightNumbers) = unzip results
-  let sortedLeftNumber = sort leftNumbers
-  let sortedRightNumber = sort rightNumbers
-  let sortedNumbers = zip sortedLeftNumber sortedRightNumber
-  let processedNumbers = map processNumbers sortedNumbers
+  result <- withFile "input.txt" ReadMode processFile 
+  putStrLn $ "Result: " ++ show result
 
-  print (sum ( map fromIntegral processedNumbers))
+processFile :: Handle -> IO Int 
+processFile h = do 
+  numbers <- readNumbers h ([],[])
+  -- INFO: 
+  -- Just learned that I could've declared multiple values in
+  -- a single `let` statement, the more you know.
+  let (leftNumbers, rightNumbers) = numbers
+      sortedNumbers = zip (sort leftNumbers) (sort rightNumbers)
+      -- INFO: This is a beauty.
+      results = map (abs . uncurry(-)) sortedNumbers
+  return $ sum results
 
-  where 
-    -- processLine :: String -> ([Int], [Int]) -> ([Int], [Int])
-    --processLine :: String -> IO ()
-    processLine :: String -> (Int, Int)
-    processLine line = 
-      let [n1, n2] = map read (words line)
-      in (n1, n2)
-    processNumbers :: (Int, Int) -> Int
-    processNumbers (n1, n2) = abs (n1 - n2)
+readNumbers :: Handle -> ([Int], [Int]) -> IO ([Int], [Int])
+readNumbers h acc = do 
+  isEOF <- hIsEOF h
+  if isEOF 
+    then return acc 
+  else do 
+    line <- hGetLine h 
+    let (n1, n2) = processLine line
+    readNumbers h (appendLists acc n1 n2)
+    where
+      processLine :: String -> (Int, Int)
+      processLine line = 
+            let [n1, n2] = map read (words line)
+            in (n1, n2)
+
+      appendLists :: ([Int], [Int]) -> Int -> Int -> ([Int], [Int])
+      appendLists (leftNumbers, rightNumbers) n1 n2 = (n1:leftNumbers, n2:rightNumbers)
 
 todo :: a 
 -- Todo implements a similar `todo!()` from Rust.
